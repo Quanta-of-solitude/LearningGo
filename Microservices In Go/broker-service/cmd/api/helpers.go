3fa09379b1246e918d1b3e0e406ca60764b61030
Sanjeev Kumar Bharadwaj <34452724+Quanta-of-solitude@users.noreply.github.com>
2022-12-01 21:18:55 +0530

    frontend & broker

---
+++ Microservices In Go/broker-service/cmd/api/helpers.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type jsonResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
+
+func (app *Config) readJSON(w http.ResponseWriter, r *http.Request, data interface{}) error {
+	maxBytes := 1048576 // one megabyte , we are limiting the size of the json response using this
+
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+	err := dec.Decode(data)
+
+	if err != nil {
+		return err
+	}
+
+	err = dec.Decode(&struct{}{})
+}
